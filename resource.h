aa97f8a004f1a415569e3a9c7f9e3a1fdb22ef12
Synhro2001 <82532564+Synhro2001@users.noreply.github.com>
2022-01-21 11:14:43 +0200

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Krasovskis_EKS.rc
+//
+#define IDD_MAINDIALOG                  101
+#define IDC_COMANDLINE                  1002
+#define IDC_START                       1003
+#define IDC_TERMINATE                   1004
+#define IDC_COMANDLINE2                 1005
+#define IDC_WRITETEXT                   1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
